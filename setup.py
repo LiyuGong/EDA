9bfbd8b19876acf5e709adc392a922f5c5bf7443
LiyuGong <wallykung@qq.com>
2018-09-21 16:56:08 -0500

    add setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='eda',
       version='1.0',
       description='eda',
+      url='xxx',
       author='xxx',
       author_email='xxx',
-      url='xxx',
-      packages=['distutils', 'distutils.command'],
-     )
+      license='MIT',
+      packages=['eda'],
+      zip_safe=False)
